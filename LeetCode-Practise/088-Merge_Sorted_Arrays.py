27d76a44de47cbee38492d5b310761f487b6f50e
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-08-02 12:40:05 +0530

    Create 088-Merge_Sorted_Arrays.py

---
+++ LeetCode-Practise/088-Merge_Sorted_Arrays.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        a, b, write_index = m-1, n-1, m+n-1
+        
+        while b>=0:
+            if a>=0 and nums1[a] > nums2[b]:
+                nums1[write_index] = nums1[a]
+                a -= 1
+            else:
+                nums1[write_index] = nums2[b]
+                b -= 1
+            write_index -= 1
+        
