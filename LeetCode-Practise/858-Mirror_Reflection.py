5a5516b7a8c25d192164ebcf43943284ab61b1cb
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-08-04 13:34:39 +0530

    Update 858-Mirror_Reflection.py

--- LeetCode-Practise/858-Mirror_Reflection.py
+++ LeetCode-Practise/858-Mirror_Reflection.py
@@ -1,4 +1,5 @@
 # Explanation: https://leetcode.com/problems/mirror-reflection/discuss/2376191/C%2B%2B-Java-Python-or-Faster-then-100-or-Full-explanations-or
+# Explanation2: https://leetcode.com/problems/mirror-reflection/discuss/2376176/C%2B%2Boror-JAVA-oror-PYTHON-oror100-Faster-Solution-Explained
 
 class Solution:
     def mirrorReflection(self, p: int, q: int) -> int:
