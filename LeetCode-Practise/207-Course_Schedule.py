3484d3a3b422a9cb4693db187eac03d025528395
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-08-19 20:10:50 +0530

    Create 207-Course_Schedule.py

---
+++ LeetCode-Practise/207-Course_Schedule.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        graph = [[] for _ in xrange(numCourses)]
+        visit = [0 for _ in xrange(numCourses)]
+        for x, y in prerequisites:
+            graph[x].append(y)
+        def dfs(i):
+            if visit[i] == -1:
+                return False
+            if visit[i] == 1:
+                return True
+            visit[i] = -1
+            for j in graph[i]:
+                if not dfs(j):
+                    return False
+            visit[i] = 1
+            return True
+        for i in xrange(numCourses):
+            if not dfs(i):
+                return False
+        return True
