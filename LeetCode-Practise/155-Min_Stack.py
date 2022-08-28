c5d04037630b9c12dff235d4fd2238e8763d932c
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-08-28 10:46:40 +0530

    Update 155-Min_Stack.py

--- LeetCode-Practise/155-Min_Stack.py
+++ LeetCode-Practise/155-Min_Stack.py
@@ -22,3 +22,47 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.stack[-1][1] if self.stack else None
+
+     
+     ## USing Linked List
+     class Node:
+    def __init__(self, val=None, mini=None, next=None):
+        # Initialize Node
+        self.val = val
+        self.minimum = mini
+        self.next = next
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.head = None
+        
+
+    def push(self, x: int) -> None:
+	# insert at beginning of the list
+	# check if head is Null
+        if self.head is None:
+            node = Node(x, x)
+            self.head = node
+        else:
+		# using min inbuilt function to check for minimum value between the current inserted value and previous minimum value
+            node = Node(x, min(x, self.head.minimum), self.head)
+            self.head = node
+        
+
+    def pop(self) -> None:
+	# just change the head pointer to next node
+        self.head = self.head.next
+        
+
+    def top(self) -> int:
+	# returns top of the linked list
+        return self.head.val
+        
+
+    def getMin(self) -> int:
+	# returns minimum value
+        return self.head.minimum
