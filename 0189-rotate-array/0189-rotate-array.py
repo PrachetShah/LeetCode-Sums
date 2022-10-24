84ebb03dc604a889b29b55b98069f575b40f6919
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-10-24 16:08:33 +0530

    Time: 596 ms (36.08%), Space: 25.4 MB (29.55%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,10 +4,29 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # Using O(n) time and O(n) space
-        val = nums[-1]
-        a = [0]*len(nums)
-        for i in range(len(nums)):
-            a[(i+k)%len(nums)] = nums[i]
+#         val = nums[-1]
+#         a = [0]*len(nums)
+#         for i in range(len(nums)):
+#             a[(i+k)%len(nums)] = nums[i]
         
-        for i in range(len(nums)):
-            nums[i] = a[i]
+#         for i in range(len(nums)):
+#             nums[i] = a[i]
+        
+        # Using Cyclic Replacements
+        k = k % len(nums)
+        count, start = 0, 0
+        while count < len(nums):
+            current = start
+            prev = nums[start]
+            
+            while True:
+                next = (current+k)%len(nums)
+                temp = nums[next]
+                nums[next] = prev
+                prev = temp
+                current = next
+                count += 1
+                
+                if start == current:
+                    break
+            start += 1
