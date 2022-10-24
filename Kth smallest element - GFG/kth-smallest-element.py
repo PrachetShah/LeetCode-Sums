32759f6a585de0892b4ec21bc5376085769e2d15
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-10-24 10:15:54 +0530

    Update kth-smallest-element.py

--- Kth smallest element - GFG/kth-smallest-element.py
+++ Kth smallest element - GFG/kth-smallest-element.py
@@ -1,30 +1,38 @@
-#User function Template for python3
-
+import sys
+sys.setrecursionlimit(10**8)
 class Solution:
-    def kthSmallest(self,arr, l, r, k):
-        '''
-        arr : given array
-        l : starting index of the array i.e 0
-        r : ending index of the array i.e size-1
-        k : find kth smallest element and return using this function
-        '''
-        arr.sort()
-        return arr[k-1]
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-#contributed by RavinderSinghPB
-if __name__ == '__main__': 
-    import random 
-    t=int(input())
-    for tcs in range(t):
-        n=int(input())
-        arr=list(map(int,input().strip().split()))
-        k=int(input())
-        ob=Solution()
-        print(ob.kthSmallest(arr, 0, n-1, k))
-        
-# } Driver Code Ends
+    def swap(self,arr, a, b): 
+        temp = arr[a] 
+        arr[a] = arr[b] 
+        arr[b] = temp 
+      
+    def randomPartition(self,arr, l, r): 
+        n = r - l + 1
+        pivot = int(random.random() % n)  
+        self.swap(arr, l + pivot, r)  
+        return self.partition(arr, l, r) 
+    def kthSmallest(self,arr, l, r, k): 
+        if (k > 0 and k <= r - l + 1): 
+              
+            pos = self.randomPartition(arr, l, r)  
+            if (pos - l == k - 1):  
+                return arr[pos]  
+            if (pos - l > k - 1):  
+                                 
+                return self.kthSmallest(arr, l, pos - 1, k)  
+      
+            
+            return self.kthSmallest(arr, pos + 1, r,  
+                               k - pos + l - 1) 
+      
+        return 999999999999
+      
+    def partition(self,arr, l, r): 
+        x = arr[r] 
+        i = l 
+        for j in range(l, r): 
+            if (arr[j] <= x): 
+                self.swap(arr, i, j)  
+                i += 1
+        self.swap(arr, i, r)  
+        return i 
