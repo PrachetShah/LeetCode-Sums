5818a4d5f4e6d16657dce0f5f3dc4e0259566ca8
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-11-07 16:46:39 +0530

    Time: 58 ms (41.91%), Space: 13.8 MB (54.69%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -2,7 +2,7 @@ class Solution:
     def maximum69Number (self, num: int) -> int:
         num = list(str(num))
         if '6' not in num:
-            return int(''.join(char for char in num))
+            return int(''.join(num))
         rep = num.index('6')
         num[rep] = '9'
-        return int(''.join(char for char in num))
+        return int(''.join(num))
