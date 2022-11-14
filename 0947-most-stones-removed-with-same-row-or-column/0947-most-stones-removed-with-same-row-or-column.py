197f2dc6adf678c70f73a765f00248d4f2d9b921
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-11-14 19:58:26 +0530

    Time: 242 ms (83.15%), Space: 14.8 MB (56.91%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -1,48 +1,21 @@
-class DSU:
-    def __init__(self, n):
-        self.parents = [i for i in range(n)]
-    def find(self, x):
-        if x != self.parents[x]:
-            self.parents[x] = self.find(self.parents[x])
-        return self.parents[x]
-    def union(self, x, y):
-        px, py = self.find(x), self.find(y)
-        if px != py:
-            self.parents[py] = px
-    def is_connected(self, x, y):
-        return self.find(x) == self.find(y)
-    def get_groups(self):
-        # return len(set(self.parents))
-        return set([self.find(x) for x in range(len(self.parents))])
-
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
-        n = len(stones)
-        dsu = DSU(n)
-        rows = {}
-        cols = {}
-        for idx in range(n):
-            i, j = stones[idx]
-            # print(idx)
-            if i in rows or j in cols:
-                prev_idx = -1
-                # if i in rows:
-                #     prev_idx = rows.get(i, None)
-                # if j in cols:
-                #     prev_idx = cols.get(j, None)
-                # # print(idx, prev_idx)
-                # dsu.union(prev_idx, idx)
-                if i in rows:
-                    prev_idx = rows.get(i, None)
-                    dsu.union(prev_idx, idx) # FIX!
-                if j in cols:
-                    prev_idx = cols.get(j, None)
-                    dsu.union(prev_idx, idx)                
-                rows[i] = cols[j] = prev_idx
-            else:
-                rows[i] = cols[j] = idx
-            # print(rows, cols)
-        
-        cnt_groups = len(dsu.get_groups())
-        # print(dsu.parents)
-        return n - cnt_groups
+        x,y=defaultdict(list),defaultdict(list)
+        for i in stones:
+            x[i[0]].append(i[1])
+            y[i[1]].append(i[0])
+        neighbour,visited=0,set()
+        for i in stones:
+            if (i[0],i[1]) not in visited:
+                q=deque()
+                q.append(i)
+                while q:
+                    stonex,stoney=q.popleft()
+                    if (stonex,stoney) not in visited:
+                        visited.add((stonex,stoney))
+                        for j in x[stonex]:
+                            q.append([stonex,j])
+                        for j in y[stoney]:
+                            q.append([j,stoney])
+                neighbour+=1
+        return len(stones)-neighbour
