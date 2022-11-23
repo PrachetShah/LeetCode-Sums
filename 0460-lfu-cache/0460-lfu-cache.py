e304a3ebef319b8e7ff7f43d1b1cd33f2d671427
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-11-23 16:22:07 +0530

    Time: 850 ms (95.11%), Space: 79.4 MB (19.68%) - LeetHub

---
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -0,0 +1,67 @@
+from collections import defaultdict
+from collections import OrderedDict
+
+class Node:
+    def __init__(self, key, val, count):
+        self.key = key
+        self.val = val
+        self.count = count
+    
+class LFUCache(object):
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.cap = capacity
+        self.key2node = {}
+        self.count2node = defaultdict(OrderedDict)
+        self.minCount = None
+        
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.key2node:
+            return -1
+        
+        node = self.key2node[key]
+        del self.count2node[node.count][key]
+        
+        # clean memory
+        if not self.count2node[node.count]:
+            del self.count2node[node.count]
+        
+        node.count += 1
+        self.count2node[node.count][key] = node
+        
+        # NOTICE check minCount!!!
+        if not self.count2node[self.minCount]:
+            self.minCount += 1
+            
+            
+        return node.val
+        
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if not self.cap:
+            return 
+        
+        if key in self.key2node:
+            self.key2node[key].val = value
+            self.get(key) # NOTICE, put makes count+1 too
+            return
+        
+        if len(self.key2node) == self.cap:
+            # popitem(last=False) is FIFO, like queue
+            # it return key and value!!!
+            k, n = self.count2node[self.minCount].popitem(last=False) 
+            del self.key2node[k] 
+        
+        self.count2node[1][key] = self.key2node[key] = Node(key, value, 1)
+        self.minCount = 1
+        return
