e56211464cf35ca4bff8c25cf4879720a20e8b41
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-12-07 08:48:23 +0530

    Time: 145 ms (86.39%), Space: 64.7 MB (60.79%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -1,29 +1,9 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int sum=0;
-    int inorder(TreeNode* root,int L,int R)
-    {
-        if(root){
-        inorder(root->left,L,R);
-        if(root->val>=L && root->val<=R)
-            sum+=root->val;
-        inorder(root->right,L,R);
-        }
-        return sum;
-    }
-    int rangeSumBST(TreeNode* root, int L, int R) {
-        if(!root)return 0;
-        return inorder(root,L,R);
+    int rangeSumBST(TreeNode* root, int L, int H) {
+        if(!root) return 0;
+        return  (root -> val >= L && root -> val <= H ? root -> val : 0) +  // add root's value if it lies within [L, H]
+                rangeSumBST(root -> left, L, H) +   // recurse left
+                rangeSumBST(root -> right, L, H);   // recurse right
     }
 };
