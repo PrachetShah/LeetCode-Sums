8a16ce33c695ff16cc1137ec8f01e3db9e8392cb
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-12-15 21:17:13 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.py
+++ Kadane's Algorithm - GFG/kadanes-algorithm.py
@@ -4,13 +4,11 @@ class Solution:
     ##Complete this function
     #Function to find the sum of contiguous subarray with maximum sum.
     def maxSubArraySum(self,arr,N):
-        ##Your code here
         max_so_far, max_total = float('-inf'), float('-inf')
         for num in arr:
-            max_so_far = max(max_so_far + num, num)
+            max_so_far = max(num, max_so_far+num)
             max_total = max(max_total, max_so_far)
         return max_total
-
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
