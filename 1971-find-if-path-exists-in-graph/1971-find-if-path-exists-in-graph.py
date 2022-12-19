038217fc922d95ab53e9e3318713d1302b7bd21f
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-12-19 11:38:35 +0530

    Time: 3128 ms (67.01%), Space: 104.8 MB (79.17%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,26 +1,25 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        # Store all edges in 'graph'.
+        # Store all edges according to nodes in 'graph'.
         graph = collections.defaultdict(list)
         for a, b in edges:
             graph[a].append(b)
             graph[b].append(a)
         
-        # Store all the nodes to be visited in 'queue'.
+        # Start from source node, add it to stack.
         seen = [False] * n
         seen[source] = True
-        queue = collections.deque([source])
-    
-        while queue:
-            curr_node = queue.popleft()
-            if curr_node == destination:
-                return True
-
-            # For all the neighbors of the current node, if we haven't visit it before,
-            # add it to 'queue' and mark it as visited.
+        stack = [source]
+        
+        while stack:
+            curr_node = stack.pop()
+            # Add all unvisited neighbors of the current node to stack 
+            # and mark them as visited.
             for next_node in graph[curr_node]:
+                if next_node == destination:
+                    return True
                 if not seen[next_node]:
                     seen[next_node] = True
-                    queue.append(next_node)
+                    stack.append(next_node)
         
-        return False
+        return seen[destination]
