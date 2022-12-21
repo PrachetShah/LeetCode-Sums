e87bdd6a3b7406eaacf8cfd85d48f78d2307d28e
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-12-21 15:14:22 +0530

    Time: 2004 ms (22.37%), Space: 21.8 MB (39.12%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,17 +1,13 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        def bfs(source):
-            q = deque([source])
-            color[source] = 0 # Start with marking source as 'RED'
-            while q:
-                node = q.popleft()
-                for neighbor in adj[node]:
-                    # If there is a conflict, return false.
-                    if color[neighbor] == color[node]: return False
-                    if color[neighbor] == -1:
-                        color[neighbor] = 1 - color[node]
-                        q.append(neighbor)
-            
+        def dfs(node, node_color):
+            color[node] = node_color
+            for neighbor in adj[node]:
+                if color[neighbor] == color[node]: return False
+                if color[neighbor] == -1:
+                    if not dfs(neighbor, 1 - node_color):
+                        return False
+
             return True
         
         adj = [[] for _ in range(n + 1)]
@@ -22,8 +18,8 @@ class Solution:
         color = [-1] * (n + 1) # 0 stands for red and 1 stands for blue.
         for i in range(1, n + 1):
             if color[i] == -1:
-                # For each pending component, run BFS.
-                if not bfs(i):
+                # For each pending component, run DFS.
+                if not dfs(i, 0):
                     # Return false, if there is conflict in the component.
                     return False
         
