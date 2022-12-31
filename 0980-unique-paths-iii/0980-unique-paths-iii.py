eddce7d579736dffb64bbef6cacfcfd909d9d966
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2022-12-31 12:49:41 +0530

    Time: 49 ms (97.37%), Space: 13.9 MB (54.63%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def uniquePathsIII(self, A):
+        self.res = 0
+        m, n, empty = len(A), len(A[0]), 1
+        for i in range(m):
+            for j in range(n):
+                if A[i][j] == 1:
+                    x, y = (i, j)
+                elif A[i][j] == 0:
+                    empty += 1
+
+        def dfs(x, y, empty):
+            if not (0 <= x < m and 0 <= y < n and A[x][y] >= 0): return
+            if A[x][y] == 2:
+                self.res += empty == 0
+                return
+            A[x][y] = -2
+            dfs(x + 1, y, empty - 1)
+            dfs(x - 1, y, empty - 1)
+            dfs(x, y + 1, empty - 1)
+            dfs(x, y - 1, empty - 1)
+            A[x][y] = 0
+        dfs(x, y, empty)
+        return self.res
+        
