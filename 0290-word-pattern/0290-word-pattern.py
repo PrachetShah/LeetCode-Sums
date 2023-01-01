ae2260414deef1b7ba2caed4ff9e21e5d9cd3c2e
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-01-01 09:54:01 +0530

    Time: 32 ms (85.55%), Space: 13.8 MB (74.33%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,14 +1,17 @@
 class Solution:
-    def wordPattern(self, p: str, s: str) -> bool:
-        words, w_to_p = s.split(' '), dict()
-
-        if len(p) != len(words): return False
-        if len(set(p)) != len(set(words)): return False # for the case w = ['dog', 'cat'] and p = 'aa'
-
-        for i in range(len(words)):
-            if words[i] not in w_to_p: 
-                w_to_p[words[i]] = p[i]
-            elif w_to_p[words[i]] != p[i]: 
-                return False
-
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        n = len(pattern)
+        words = s.split(' ')
+        sim = {}
+        if len(words) != n:
+            return False
+        if len(set(words)) != len(set(pattern)):
+            return False
+        
+        for i in range(len(pattern)):
+            if pattern[i] in sim.keys():
+                if sim[pattern[i]] != words[i]:
+                    return False
+            else:
+                sim[pattern[i]] = words[i]
         return True
