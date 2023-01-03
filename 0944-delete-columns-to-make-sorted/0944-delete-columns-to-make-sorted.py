91a3ee640e46b2e534304ad06d1a9d8a30bc0bb7
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-01-03 18:09:48 +0530

    Time: 212 ms (64.00%), Space: 14.6 MB (61.89%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        cols = len(strs[0])
+        ans = 0
+        for i in range(cols):
+            order = ord('a')
+            for j in range(len(strs)):
+                if order > ord(strs[j][i]):
+                    ans += 1
+                    break
+                else:
+                    order = ord(strs[j][i])
+        # print(ans)
+        return ans
