7e4ec7a9ca54d88bca530dde34675ec0d0442b7f
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-01-03 18:13:01 +0530

    Time: 636 ms (16.00%), Space: 14.7 MB (25.26%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -3,12 +3,12 @@ class Solution:
         cols = len(strs[0])
         ans = 0
         for i in range(cols):
-            order = ord('a')
+            # order = ord('a')
             for j in range(1, len(strs)):
                 if ord(strs[j-1][i]) > ord(strs[j][i]):
                     ans += 1
                     break
-                else:
-                    order = ord(strs[j][i])
+                # else:
+                #     order = ord(strs[j][i])
         # print(ans)
         return ans
