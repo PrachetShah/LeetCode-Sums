8016de60c19e206d5b069cc296cd9ef02da94947
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-02-01 08:55:47 +0530

    Time: 3 ms (83.39%), Space: 7.2 MB (43.04%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2 != str2+str1){
+            return "";
+        }
+        int gcdLength = gcd(str1.size(), str2.size());
+        return str1.substr(0, gcdLength);
+    }
+};
