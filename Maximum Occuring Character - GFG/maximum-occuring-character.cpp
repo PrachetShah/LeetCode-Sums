81b9a920fc1cdbb5409ee545d677da96fa7e9209
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-02-21 17:12:28 +0530

    Adding Solution - LeetHub

--- Maximum Occuring Character - GFG/maximum-occuring-character.cpp
+++ Maximum Occuring Character - GFG/maximum-occuring-character.cpp
@@ -12,23 +12,42 @@ class Solution
     //Function to find the maximum occurring character in a string.
     char getMaxOccuringChar(string str)
     {
-        // Your code here
-        map<char, int> counter;
+
+        // USING MAP
+        /* map<char, int> counter;
         
-        for(int i=0; i<str.size(); i++){
-            counter[str[i]]++;
-        }
+        // for(int i=0; i<str.size(); i++){
+        //     counter[str[i]]++;
+        // }
         
-        int max = 0;
-        char temp;
-        for(auto count : counter){
-            if(count.second > max){
-                max = count.second;
-                temp = count.first;
-            }
-        }
+        // int max = 0;
+        // char temp;
+        // for(auto count : counter){
+        //     if(count.second > max){
+        //         max = count.second;
+        //         temp = count.first;
+        //     }
+        // }
         
         return temp;
+        */
+        
+        // USING ARRAY
+        vector<int> hash(26, 0);
+        for(auto s: str){
+            hash[s-'a']++;
+        }
+        int maxi=0;
+        char temp;
+        
+        for(int i=0; i<26; i++){
+            if(hash[i] > maxi){
+                maxi = hash[i];
+                temp = i + 'a';
+            }
+        }
+        return temp;
+        
     }
 
 };
