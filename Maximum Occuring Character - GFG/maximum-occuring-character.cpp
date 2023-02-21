2ef92ff23bc5470db8474f1a0687da64d493da12
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-02-21 17:06:25 +0530

    Added solution - LeetHub

---
+++ Maximum Occuring Character - GFG/maximum-occuring-character.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find the maximum occurring character in a string.
+    char getMaxOccuringChar(string str)
+    {
+        // Your code here
+        map<char, int> counter;
+        
+        for(int i=0; i<str.size(); i++){
+            counter[str[i]]++;
+        }
+        
+        int max = 0;
+        char temp;
+        for(auto count : counter){
+            if(count.second > max){
+                max = count.second;
+                temp = count.first;
+            }
+        }
+        
+        return temp;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string str;
+        cin >> str;
+    	Solution obj;
+        cout<< obj.getMaxOccuringChar(str)<<endl;
+    }
+}
+// } Driver Code Ends
