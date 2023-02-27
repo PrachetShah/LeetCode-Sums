8e88926ad6203ba12e3548ede31eada1ea084064
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-02-27 19:10:34 +0530

    Time: 222 ms (72.06%), Space: 10.3 MB (72.82%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        // SIEVE OF ERATOSTHENES
+        int count=0;
+        vector<bool> prime(n+1, true);
+        
+        prime[0] = prime[1] = false;
+        for(int i=2; i<n; i++){
+            if(prime[i]){
+                count++;
+                for(int j=2*i; j<n; j=j+i){
+                    prime[j] = 0;
+                }
+            }
+        }
+        return count;
+    }
+};
