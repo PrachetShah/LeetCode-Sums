84a9fc9df184862905e57ba886ca460ba795cc8c
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-03-03 18:31:47 +0530

    Time: 1493 ms (7.09%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,5 +1,17 @@
 # Write your MySQL query statement below
-select employee_id from employees where employee_id not in (select employee_id from salaries)
+
+# Using UNION
+# select employee_id from employees where employee_id not in (select employee_id from salaries)
+# union
+# select employee_id from salaries where employee_id not in (select employee_id from employees)
+# order by employee_id;
+
+# Using LEFT JOIN
+select e.employee_id from employees as e
+left join salaries s on e.employee_id=s.employee_id
+where salary is null
 union
-select employee_id from salaries where employee_id not in (select employee_id from employees)
+select s.employee_id from salaries as s
+left join employees e on s.employee_id=e.employee_id
+where name is null
 order by employee_id;
