f59d3d25346861f56a30f674fda1b28e43a796af
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-03-05 18:12:20 +0530

    Time: 7 ms (80.61%), Space: 14.3 MB (5.59%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -56,3 +56,43 @@ public:
         return {first(nums, target), last(nums, target)};
     }
 };
+
+// RECUSRIVE
+
+// class Solution {
+// public:
+//     int first(vector<int> nums, int low, int high, int target){
+//         int mid = low + (high-low)/2;
+//         if(low > high)
+//             return -1;
+//         if(nums[mid] == target){
+//             while(nums[mid]==target){
+//                 mid = mid - 1;
+//             }
+//             return mid+1;
+//         }
+//         else if(nums[mid] > target)
+//             return first(nums, low, mid-1, target);
+//         else
+//             return first(nums, mid+1, high, target);
+//     }
+    
+//     int last(vector<int> nums, int low, int high, int target){
+//         int mid = low + (high-low)/2;
+//         if(low > high)
+//             return -1;
+//         if(nums[mid] == target){
+//             while(nums[mid]==target){
+//                 mid = mid + 1;
+//             }
+//             return mid-1;
+//         }
+//         else if(nums[mid] > target)
+//             return last(nums, low, mid-1, target);
+//         else
+//             return last(nums, mid+1, high, target);
+//     }
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         return {first(nums, 0, nums.size()-1, target), last(nums, 0, nums.size()-1, target)};
+//     }
+// };
