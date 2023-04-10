09395b03895bc2c9ff85dc48a661f322ba88a4e7
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-04-10 18:44:12 +0530

    Time: 4 ms (39.51%), Space: 6.4 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto i: s){
+            if(i=='(' || i=='[' || i=='{'){
+                st.push(i);
+            }else{
+                if(st.empty() || (st.top()=='(' && i!=')') || (st.top()=='[' && i!=']') || (st.top()=='{' && i!='}')){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
