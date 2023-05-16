3874352baa8e3860c87d4d24b3f57bf3b393fd0f
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-05-16 10:41:28 +0530

    Added solution - LeetHub

---
+++ Find the first node of loop in linked list - GFG/find-the-first-node-of-loop-in-linked-list.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution
+{
+    public:
+     //Function to find first node if the linked list has a loop.
+    int findFirstNode(Node* head)
+    {
+        // your code here
+        Node* curr = head;
+        map<Node*, bool> m;
+        while(curr){
+            if(m.find(curr) != m.end()){
+                return curr->data;
+            }else{
+                m[curr] = true;
+            }
+            curr = curr->next;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        int ans = ob.findFirstNode(head);
+        cout<<ans<<"\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
