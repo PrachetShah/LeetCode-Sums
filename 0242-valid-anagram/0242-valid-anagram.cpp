a196d301bc896a19c7a44ff86cf735c135d33073
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-05-28 10:30:02 +0530

    Time: 18 ms (19.66%), Space: 7.6 MB (5.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,23 +4,41 @@ public:
         if(s.length() != t.length()){
             return false;
         }
-        int len = s.length(), i;
-        map<char, int> one;
-        map<char, int> two;
-        for(i=0; i<len; i++){
-            one[s[i]]++;
-            two[t[i]]++;
+        int len = s.length();
+        map<char, int> counts;
+        for(int i=0; i<len; i++){
+            counts[s[i]]++;
+            counts[t[i]]--;
         }
-        // for(i=0; i<len; i++){
-        //     if(one[s[i]] != two[s[i]]){
-        //         return false;
-        //     }
-        // }
-        if(one == two){
-            return true;
-        }else{
-            return false;
+        for(auto count: counts){
+            if(count.second){
+                return false;
+            }
         }
-        // return true;
+        return true;
+        
+        
+//         int len = s.length(), i;
+//         map<char, int> one;
+//         map<char, int> two;
+//         for(i=0; i<len; i++){
+//             one[s[i]]++;
+//             two[t[i]]++;
+//         }
+        
+// //         M1
+//         if(one == two){
+//             return true;
+//         }else{
+//             return false;
+//         }
+// //         M2
+//         // for(i=0; i<len; i++){
+//         //     if(one[s[i]] != two[s[i]]){
+//         //         return false;
+//         //     }
+//         // }
+        
+//         // return true;
     }
 };
