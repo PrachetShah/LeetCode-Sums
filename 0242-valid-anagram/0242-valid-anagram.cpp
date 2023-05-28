10fe170a19d5a867749d06363644029a4dc09e70
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-05-28 10:25:30 +0530

    Time: 11 ms (59.58%), Space: 7.5 MB (14.29%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        int len = s.length(), i;
+        map<char, int> one;
+        map<char, int> two;
+        for(i=0; i<len; i++){
+            one[s[i]]++;
+            two[t[i]]++;
+        }
+        for(i=0; i<len; i++){
+            if(one[s[i]] != two[s[i]]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
