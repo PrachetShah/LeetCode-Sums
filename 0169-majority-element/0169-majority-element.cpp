a6938880957221cd69c92b085b35419985670876
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-06-08 18:21:52 +0530

    Time: 8 ms (99.12%), Space: 19.5 MB (62.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> counter;
+//         Boyer-Moore Voting
+        int count = 0;
+        int candidate = 0;
         for(int i=0; i<nums.size(); i++){
-            counter[nums[i]]++;
-        }
-        int num = nums[0];
-        for(auto i: counter){
-            if(counter[num] < counter[i.first]){
-                num = i.first;
+            if(count == 0){
+                candidate = nums[i];
             }
+            count += (nums[i]==candidate)?1:-1;
         }
-        return num;
+        return candidate;
+//         Hashmap
+        // map<int, int> counter;
+        // for(int i=0; i<nums.size(); i++){
+        //     counter[nums[i]]++;
+        // }
+        // int num = nums[0];
+        // for(auto i: counter){
+        //     if(counter[num] < counter[i.first]){
+        //         num = i.first;
+        //     }
+        // }
+        // return num;
     }
 };
