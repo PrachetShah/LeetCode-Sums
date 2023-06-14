7fc11b0c5cd4ec4803150040238ea3e3413311e1
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-06-14 21:50:09 +0530

    Time: 3 ms (85.08%), Space: 9.5 MB (27.80%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,19 +5,18 @@ public:
         int col = matrix[0].size();
         
         int start = 0;
-        int end = row*col-1;
+        int end = row*col - 1;
         
         int mid = start + (end-start)/2;
         
-        while(start<= end){
+        while(start <= end){
             int element = matrix[mid/col][mid%col];
-            
             if(element == target){
                 return true;
-            }else if(element < target){
-                start = mid + 1;
-            }else{
+            }else if(element > target){
                 end = mid - 1;
+            }else{
+                start = mid + 1;
             }
             mid = start + (end-start)/2;
         }
