5399c481bbdf0c034b3386cb29587c74efb0a09f
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-06-22 21:48:08 +0530

    Added solution - LeetHub

--- Merge Sort - GFG/merge-sort.cpp
+++ Merge Sort - GFG/merge-sort.cpp
@@ -19,29 +19,25 @@ void printArray(int arr[], int size)
 class Solution
 {
     public:
-    void merge(int array[], int const left, int const mid,
-           int const right)
-{
-    auto const subArrayOne = mid - left + 1;
-    auto const subArrayTwo = right - mid;
-  
+    void merge(int array[], int left, int mid, int right)
+    {
+         // Your code here
+         int const subArrayOne = mid - left + 1;
+    int const subArrayTwo = right - mid;
+ 
     // Create temp arrays
     auto *leftArray = new int[subArrayOne],
          *rightArray = new int[subArrayTwo];
-  
+ 
     // Copy data to temp arrays leftArray[] and rightArray[]
     for (auto i = 0; i < subArrayOne; i++)
         leftArray[i] = array[left + i];
     for (auto j = 0; j < subArrayTwo; j++)
         rightArray[j] = array[mid + 1 + j];
-  
-    auto indexOfSubArrayOne
-        = 0, // Initial index of first sub-array
-        indexOfSubArrayTwo
-        = 0; // Initial index of second sub-array
-    int indexOfMergedArray
-        = left; // Initial index of merged array
-  
+ 
+    auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+ 
     // Merge the temp arrays back into array[left..right]
     while (indexOfSubArrayOne < subArrayOne
            && indexOfSubArrayTwo < subArrayTwo) {
@@ -58,6 +54,7 @@ class Solution
         }
         indexOfMergedArray++;
     }
+ 
     // Copy the remaining elements of
     // left[], if there are any
     while (indexOfSubArrayOne < subArrayOne) {
@@ -66,6 +63,7 @@ class Solution
         indexOfSubArrayOne++;
         indexOfMergedArray++;
     }
+ 
     // Copy the remaining elements of
     // right[], if there are any
     while (indexOfSubArrayTwo < subArrayTwo) {
@@ -76,21 +74,20 @@ class Solution
     }
     delete[] leftArray;
     delete[] rightArray;
-}
-  
-// begin is for left index and end is
-// right index of the sub-array
-// of arr to be sorted */
-void mergeSort(int array[], int const begin, int const end)
-{
-    if (begin >= end)
-        return; // Returns recursively
-  
-    auto mid = begin + (end - begin) / 2;
-    mergeSort(array, begin, mid);
-    mergeSort(array, mid + 1, end);
-    merge(array, begin, mid, end);
-}
+    }
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        if(l>=r){
+            return;
+        }
+        int mid = l + (r-l)/2;
+        
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        merge(arr, l,mid,  r);
+    }
 };
 
 //{ Driver Code Starts.
