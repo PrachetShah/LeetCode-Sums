4f9cb7d93d749d17548f069fbc2c3c4f3dd1ebfa
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-09-04 16:08:56 +0530

    Time: 7 ms (91.08%), Space: 8.2 MB (36.51%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,16 +9,28 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-       ListNode* first = head;
-       map<ListNode*, bool> seen;
-       while(head){
-           if(seen.find(head) != seen.end()){
-               return true;
-           }else{
-               seen[head] = true;
-           }
-           head = head->next;
-       }
-       return false;
+       // ListNode* first = head;
+       // map<ListNode*, bool> seen;
+       // while(head){
+       //     if(seen.find(head) != seen.end()){
+       //         return true;
+       //     }else{
+       //         seen[head] = true;
+       //     }
+       //     head = head->next;
+       // }
+       // return false;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
     }
 };
