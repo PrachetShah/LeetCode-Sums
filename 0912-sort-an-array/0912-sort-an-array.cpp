cbb034af9b250098045f6588298633eff34a0a9c
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-10-23 20:20:48 +0530

    Time: 155 ms (70.50%), Space: 72.2 MB (53.76%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
+        cin.tie(NULL);
         priority_queue<int, vector<int>, greater<int>> q;
         for(int i=0; i<nums.size(); i++){
             q.push(nums[i]);
