775ab24ead6616178841c6b58baed02bdc433b40
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-10-23 20:21:09 +0530

    Time: 121 ms (79.34%), Space: 72.4 MB (53.42%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         priority_queue<int, vector<int>, greater<int>> q;
         for(int i=0; i<nums.size(); i++){
