35c0b0ca7008842f882ee5d9dc4fb154f2ee3b1e
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2023-10-23 20:20:27 +0530

    Time: 141 ms (76.61%), Space: 72.2 MB (53.76%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,31 +1,15 @@
 class Solution {
-    void countingSort(vector<int> &arr) {
-        // Create the counting hash map.
-        unordered_map<int, int> counts;
-        // Find the minimum and maximum values in the array.
-        int minVal = *min_element(arr.begin(), arr.end());
-        int maxVal = *max_element(arr.begin(), arr.end());
-
-        // Update element's count in the hash map.
-        for (auto& val : arr) {
-            counts[val]++;
-        }
-        
-        int index = 0;
-        // Place each element in its correct position in the array.
-        for (int val = minVal; val <= maxVal; ++val) {
-            // Append all 'val's together if they exist.
-            while (counts[val] > 0) {
-                arr[index] = val;
-                index += 1;
-                counts[val] -= 1;
-            }
-        }
-    }
-
 public:
     vector<int> sortArray(vector<int>& nums) {
-        countingSort(nums);
+        priority_queue<int, vector<int>, greater<int>> q;
+        for(int i=0; i<nums.size(); i++){
+            q.push(nums[i]);
+        }
+        
+        for(int i=0; i<nums.size(); i++){
+            nums[i] = q.top();
+            q.pop();
+        }
         return nums;
     }
 };
