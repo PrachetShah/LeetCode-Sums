d7feb29828dc9f60c8af03c20a6a9abb7f2508fd
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2024-03-09 00:55:53 +0530

    Time: 164 ms (20.30%), Space: 18.7 MB (57.30%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        return_array = [0] * len(A)
+        write_pointer = len(A) - 1
+        left_read_pointer = 0
+        right_read_pointer = len(A) - 1
+        left_square = A[left_read_pointer] ** 2
+        right_square = A[right_read_pointer] ** 2
+        while write_pointer >= 0:
+            if left_square > right_square:
+                return_array[write_pointer] = left_square
+                left_read_pointer += 1
+                left_square = A[left_read_pointer] ** 2
+            else:
+                return_array[write_pointer] = right_square
+                right_read_pointer -= 1
+                right_square = A[right_read_pointer] ** 2
+            write_pointer -= 1
+        return return_array
