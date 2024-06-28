41ec6029f255c90ddae2270173391a18b1862c1d
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2024-06-28 22:24:51 +0530

    Time: 0 ms, Memory: 41.2 MB - LeetHub

--- plus-one/plus-one.java
+++ plus-one/plus-one.java
@@ -1,5 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1; i>=0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] out = new int[digits.length+1];
+        out[0] = 1;
+        return out;
+        
+        
+        /*
+        MY INITIAL SOL
         if(digits[digits.length-1] < 9){
             digits[digits.length-1]++;
             return digits;
@@ -21,5 +35,6 @@ class Solution {
             }
 
         }
+        */
     }
 }
