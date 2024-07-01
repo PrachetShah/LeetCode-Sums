1341d552d21314780baabd51fe47bc8426d3726a
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2024-07-01 22:00:30 +0530

    Time: 1 ms, Memory: 48.7 MB - LeetHub

--- reverse-string/reverse-string.java
+++ reverse-string/reverse-string.java
@@ -1,9 +1,18 @@
 class Solution {
     public void reverseString(char[] s) {
+        int n = s.length;
         for(int i=0; i<s.length/2; i++){
+            // using a temp third var
+            /*
             char temp = s[i];
             s[i] = s[s.length-i-1];
             s[s.length-i-1] = temp;
+            */
+            
+            // swapping using xor
+            s[i] = (char) (s[i]^s[n-i-1]);
+            s[n-i-1] = (char) (s[i]^s[n-i-1]);
+            s[i] = (char) (s[i]^s[n-i-1]);
         }
     }
 }
