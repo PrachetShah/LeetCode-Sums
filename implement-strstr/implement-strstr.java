31b38d12218c9073589c1f0134da59c4239205e4
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2024-07-09 23:21:33 +0530

    Time: 1 ms, Memory: 41.4 MB - LeetHub

---
+++ implement-strstr/implement-strstr.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length() > haystack.length()){
+            return -1;
+        }
+ 
+        List<Integer> indexes = new ArrayList<>();
+        
+        for(int i=0; i<haystack.length() - needle.length() + 1; i++){
+            if(haystack.charAt(i) == needle.charAt(0)){
+                indexes.add(i);
+            }
+        }
+        
+        for(int index: indexes){
+            int start = index;
+            boolean isBreak = false;
+            for(int i=0; i<needle.length(); i++){
+                if(haystack.charAt(start) != needle.charAt(i)){
+                    isBreak = true;
+                    break;
+                }else{
+                    start++;
+                }
+            }
+            if(!isBreak){
+                return index;
+            }
+        }
+        
+        return -1;
+    }
+}
