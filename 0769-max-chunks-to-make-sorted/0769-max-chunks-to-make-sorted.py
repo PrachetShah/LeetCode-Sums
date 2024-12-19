e3c785a8b1f1037c88271d6b91cbf452e64157a8
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2024-12-19 22:20:51 +0530

    Time: 0 ms (100.00%), Space: 17.7 MB (14.45%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
@@ -1,17 +1,14 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         n = len(arr)
-        prefixMax = arr[:]
-        suffixMin = arr[:]
-        
-        for i in range(1, n):
-            prefixMax[i] = max(prefixMax[i], prefixMax[i-1])
-        
-        for i in range(n-2, -1, -1):
-            suffixMin[i] = min(suffixMin[i], suffixMin[i+1])
-        
         chunks = 0
+        prefixSum = 0
+        sortedPrefixSum = 0
+        
         for i in range(n):
-            if i==0 or suffixMin[i]>prefixMax[i-1]:
+            prefixSum += arr[i]
+            sortedPrefixSum += i
+        
+            if prefixSum == sortedPrefixSum:
                 chunks += 1
         return chunks
