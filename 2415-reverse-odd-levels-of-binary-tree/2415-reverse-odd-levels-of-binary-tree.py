fb866b1593ebc58a4624f62131d110de32746e81
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2024-12-20 21:56:05 +0530

    Time: 91 ms (8.46%), Space: 23 MB (5.08%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -6,13 +6,40 @@
 #         self.right = right
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        isOdd = False
-        def solve(leftC, rightC, isOdd):
-            if leftC is None or rightC is None:
-                return
-            if isOdd%2 == 0:
-                leftC.val, rightC.val = rightC.val, leftC.val
-            solve(leftC.left, rightC.right, isOdd+1)
-            solve(leftC.right, rightC.left, isOdd+1)
-        solve(root.left, root.right, 0)
+#         dfs approach
+        # isOdd = False
+        # def solve(leftC, rightC, isOdd):
+        #     if leftC is None or rightC is None:
+        #         return
+        #     if isOdd%2 == 0:
+        #         leftC.val, rightC.val = rightC.val, leftC.val
+        #     solve(leftC.left, rightC.right, isOdd+1)
+        #     solve(leftC.right, rightC.left, isOdd+1)
+        # solve(root.left, root.right, 0)
+        # return root
+#         BFS Approach
+        if root is None:
+            return None
+        queue = [root]
+        level = 0
+        while queue:
+            size = len(queue)
+            currentLvl = []
+            for _ in range(size):
+                node = queue.pop(0)
+                currentLvl.append(node)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                
+            if level%2==1:
+                left, right = 0, len(currentLvl)-1
+                while left < right:
+                    currentLvl[left].val, currentLvl[right].val = currentLvl[right].val, currentLvl[left].val
+                    left += 1
+                    right -= 1
+            level += 1
         return root
+        
