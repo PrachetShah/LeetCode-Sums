e9a9e0323ef65e9819c94f8e0962243c2f5cb6a2
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2024-12-25 13:22:25 +0530

    Time: 3 ms (48.85%), Space: 19.9 MB (6.38%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
@@ -6,6 +6,23 @@
 #         self.right = right
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        # USING DFS
+        def dfs(node, depth):
+            if node is None:
+                return
+            if depth == len(ans):
+                ans.append(node.val)
+            else:
+                ans[depth] = max(node.val, ans[depth])
+            
+            dfs(node.left, depth+1)
+            dfs(node.right, depth+1)
+        ans = []
+        dfs(root, 0)
+        return ans
+        
+        # USING BFS
+        
         if root is None:
             return []
         queue = [[root]]
